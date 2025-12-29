c8fe5e3c436df6a8c8fa8630250697dfda9e17cc
abhisarshukla <abhisarshukla645@gmail.com>
2025-12-30 00:59:36 +0530

    feat: new style

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abhisar.sh',
-  integrations: [mdx(), sitemap(), react()],
-
   vite: {
-    plugins: [tailwindcss()],
-  },
+    plugins: [tailwindcss()]
+  }
 });
--- blog/src/content/blog/first-post.md
+++
@@ -1,8 +0,0 @@
----
-title: "First post"
-description: "The first blog post"
-pubDate: "May 06 2025"
-updatedDate: "Sep 12 2025"
----
-
-This is first blog post, it doesn't have any meaning or purpose, but it's a good start. This is just to test if I can deploy **this** blog and get it live.
